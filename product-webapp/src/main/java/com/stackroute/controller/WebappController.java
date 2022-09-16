15e3a1fff133fcaf3843d76bc80624cd71767ec0
adarsha14 <111411927+adarsha14@users.noreply.github.com>
2022-09-16 17:17:12 +0530

    Update WebappController.java

--- product-webapp/src/main/java/com/stackroute/controller/WebappController.java
+++ product-webapp/src/main/java/com/stackroute/controller/WebappController.java
@@ -1,4 +1,4 @@
-/*
+
 package com.stackroute.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,4 +11,4 @@ public class WebappController {
         return "index.html";
     }
 }
-*/
+
