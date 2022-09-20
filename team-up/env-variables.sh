f6f9e0efa0d5f47c60ae8d7aa1b74a7a9f6642bf
adarsha.madival <adarsha.madival@globallogic.com>
2022-09-20 10:47:34 +0530

    new commit

--- team-up/env-variables.sh
+++ team-up/env-variables.sh
@@ -1,2 +1,2 @@
 export MONGO_DATABASENAME=teamup_db
-export MONGO_URI=mongodb://localhost:27017/teamup_db
+export MONGO_URI=mongodb://34.201.191.117:27017/teamup_db
--- user-service/env-variables.sh
+++ user-service/env-variables.sh
@@ -1,2 +1,2 @@
 export MONGO_DATABASENAME=userservice_db
-export MONGO_URI=mongodb://localhost:27017/userservice_db
+export MONGO_URI=mongodb://34.201.191.117:27017/userservice_db
