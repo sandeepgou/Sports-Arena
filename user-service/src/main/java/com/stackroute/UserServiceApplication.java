1454508bb40d0586a9de8fafc179f693dc7b1213
adarsha.madival <adarsha.madival@globallogic.com>
2022-09-21 10:03:56 +0530

    new commit

--- user-service/src/main/java/com/stackroute/UserServiceApplication.java
+++ user-service/src/main/java/com/stackroute/UserServiceApplication.java
@@ -2,9 +2,10 @@ package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 @EnableEurekaClient
 public class UserServiceApplication {
 
